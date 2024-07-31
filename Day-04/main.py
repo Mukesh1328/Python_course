7155b1f55d22361a5f53dbe042197ba442a95aad
Mukesh Tanuku <39186389+Mukesh1328@users.noreply.github.com>
2024-07-31 22:30:22 +0530

    Create main.py

---
+++ Day-04/main.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+coin_side = random.randint(1,2)
+if coin_side == 1:
+  print ("Tails")
+
+else:
+  print ("Heads")
